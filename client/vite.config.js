9c877e5530018eef7c526c4fdf8a2ae880b8aa68
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-10-02 20:31:50 -0300

    fix: update vite config js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    cssMinify: 'lightningcss',
+  },
   plugins: [react()],
+  clearScreen: false,
+  server: {
+    port: 3000,
+    strictPort: false,
+  }
 })
