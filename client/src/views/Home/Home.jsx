50cb18fa28462ae0b911a41d895b878b8dcdd6ea
Paolo Suarez <ptsuarez91@hotmail.com>
2024-10-02 13:25:48 -0400

    feat(client): inicialización del proyecto con Vite, Tailwind y React Router DOM
    - Configuré el entorno del cliente con React utilizando Vite para una mayor velocidad y simplicidad.
    - Instalé y configuré Tailwind CSS para el manejo de estilos de manera eficiente.
    - Integré React Router DOM para gestionar las rutas de la aplicación.
    - Implementé una página de ERROR 404 para gestionar rutas no existentes.
    - Definí una estructura de carpetas modular y flexible en el cliente, sujeta a futuros ajustes según el avance del proyecto.
    - El servidor (server) aún no tiene configuración ni implementación.

---
+++ client/src/views/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-green-700 text-4xl bg-gray-600 text-center'>El que no trabaja se va al cielo</div>
+  )
+}
+
+export default Home
