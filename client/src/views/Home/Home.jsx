6012059edf1daeadcff1e61dfe481e138fbfc99a
PHRCOOK <pabloelleproso@gmail.com>
2024-10-10 11:29:28 -0300

    feat: modificaion de componente login

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from '../../components/loginButton/loginButon.jsx';
 import LogoutButton from '../../components/logoutButton/logoutButton.jsx';
 
 const Home = () => {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, user } = useAuth0();
+
+  useEffect(() => {
+    if (isAuthenticated && user) {
+      console.log('Usuario autenticado:', user);
+    }
+  }, [isAuthenticated, user]);
 
   return (
     <main className="flex max-w-3xl flex-col items-center justify-center">
       <div className="w-full text-center">Esta es la home</div>
-      {!isAuthenticated ? <LoginButton /> : <LogoutButton />}
+      {!isAuthenticated ? (
+        <LoginButton />
+      ) : (
+        <div>
+          <LogoutButton />
+        </div>
+      )}
     </main>
   );
 };
