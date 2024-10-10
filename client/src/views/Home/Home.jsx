3245d6a18f666fdce83d5f81af49747d52ca6454
PHRCOOK <pabloelleproso@gmail.com>
2024-10-10 10:56:06 -0300

    feat: creacion de boton logout

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LoginButton from '../../components/loginButton/loginButon.jsx';
+import LogoutButton from '../../components/logoutButton/logoutButton.jsx';
 
 const Home = () => {
   return (
     <main className="flex max-w-3xl flex-col items-center justify-center">
       <div className="w-full text-center">Esta es la home</div>
       <LoginButton />
+      <LogoutButton />
     </main>
   );
 };
