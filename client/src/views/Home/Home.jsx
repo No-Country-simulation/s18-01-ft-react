c9ff095fe9bf2200e41866253b3e5e503c8bad65
PHRCOOK <pabloelleproso@gmail.com>
2024-10-10 11:18:26 -0300

    feat: modificacion de componentes logout y login

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from '../../components/loginButton/loginButon.jsx';
 import LogoutButton from '../../components/logoutButton/logoutButton.jsx';
 
 const Home = () => {
+  const { isAuthenticated } = useAuth0();
+
   return (
     <main className="flex max-w-3xl flex-col items-center justify-center">
       <div className="w-full text-center">Esta es la home</div>
-      <LoginButton />
-      <LogoutButton />
+      {!isAuthenticated ? <LoginButton /> : <LogoutButton />}
     </main>
   );
 };
