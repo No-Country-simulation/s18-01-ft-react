ae0ef05c23b9a23b3107cab4f35fb74d2a5f31c5
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-10-10 23:58:08 -0300

    feat: create enterprise signup form

--- client/src/components/EnterpriseSignupForm/EnterpriseSignupForm.jsx
+++ client/src/components/EnterpriseSignupForm/EnterpriseSignupForm.jsx
@@ -1,10 +1,31 @@
 import FormComponent from '../FormComponent/FormComponent';
 
-export const EnterpriseSignupForm = () => {
-  return (
-    <div>
-      <Button />
-      <FormComponent />
-    </div>
-  );
-};
+/**
+ * Array que corresponde a los inputs del formulario de registro de empresa.
+ * @typedef {Object[]} EnterpriseSignupFields
+ * @property {string} label - Text del label del input
+ * @property {string} type - Tipo dle input
+ * @property {number} tabindex - El orden en el que el input recibe el foco
+ * @property {string} id - ID del elemento
+ */
+const enterpriseSignupFields = [
+  { label: 'Nombre', type: 'text', tabindex: 1, id: 'nameField' },
+  { label: 'Email', type: 'email', tabindex: 2, id: 'emailField' },
+  { label: 'Dominio', type: 'text', tabindex: 3, id: 'domainField' },
+  { label: 'Contraseña', type: 'password', tabindex: 4, id: 'passwordField' },
+];
+
+/**
+ * Formulario de registro de empresa.
+ *
+ * Este formulario solicita el nombre, email, dominio y contraseña.
+ *
+ * @returns {JSX.Element} Un formulario con los campos mencionados.
+ */
+export const EnterpriseSignupForm = () => (
+  <FormComponent
+    id="enterprise-signup"
+    btnText="Registrarme"
+    fields={enterpriseSignupFields}
+  />
+);
--- client/src/views/EnterpriseSignup/EnterpriseSignup.jsx
+++ client/src/views/EnterpriseSignup/EnterpriseSignup.jsx
@@ -1,10 +1,14 @@
+import { EnterpriseSignupForm } from '@/components/EnterpriseSignupForm/EnterpriseSignupForm';
+
 export const EnterpriseSignup = () => {
   return (
     <div className="flex w-full flex-col items-center justify-center">
       <header className="sr-only">
         <h1>Registra tu empresa</h1>
       </header>
-      <main className="mx-auto w-full max-w-3xl"></main>
+      <main className="mx-auto w-full max-w-3xl">
+        <EnterpriseSignupForm />
+      </main>
     </div>
   );
 };
