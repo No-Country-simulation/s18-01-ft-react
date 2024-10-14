7c2e493565f07ee59ff0ea242e32c82d05fe05a9
Rocio Tellez Lopez <rociotellezlopez@gmail.com>
2024-10-13 23:09:58 -0500

    feat: add toast component and update endOfLine in prettier and eslint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { Suspense } from 'react';
 import AppRouter from './routes/Router.jsx';
+import ToastNotification from './components/ToastNotification/ToastNotification.jsx';
 
 const App = () => (
   <Suspense fallback={<div>Loading...</div>}>
+    <ToastNotification />
     <AppRouter />
   </Suspense>
 );
