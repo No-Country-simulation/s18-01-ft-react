0a7cf20b1c57ab334ded1a8abde28d8df1414b50
DavidMoralesz4 <jd3285201@gmail.com>
2024-10-15 14:02:51 -0500

    feat: app-header-upgrates

--- client/src/components/AppHeader/AppHeader.jsx
+++ client/src/components/AppHeader/AppHeader.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import AppSearchBar from '../Search/AppSearchBar';
 
 function AppHeader() {
   return (
-    <div className="h-28 w-auto bg-slate-500">
-      <h1 className="flex items-center justify-center text-white">Soy Header</h1>
-      <div>
-        <div></div>
-      </div>
+    <div className="flex h-28 w-auto items-center justify-center space-x-24">
+      <button className="size-auto space-x-24 rounded-full bg-white p-2">---</button>
+      <AppSearchBar />
+      <div className="size-auto rounded-full bg-white p-2"> Avatar</div>
     </div>
   );
 }
