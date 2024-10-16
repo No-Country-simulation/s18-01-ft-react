4b8f41f638451462ca3a967588944c8689fc6989
Rocio Tellez Lopez <rociotellezlopez@gmail.com>
2024-10-16 00:07:18 -0500

    style: agrega color y border a phaserContainer

--- client/src/components/PhaserContainer/PhaserContainer.jsx
+++ client/src/components/PhaserContainer/PhaserContainer.jsx
@@ -23,11 +23,11 @@ const PhaserContainer = () => {
   }, []);
 
   return (
-    <div className="mx-auto mt-8 flex size-full max-w-4xl flex-col">
+    <div className="mx-auto mt-9 flex size-full max-w-4xl flex-col">
       <div
         ref={gameRef}
         id="office"
-        className="size-full max-h-[500px] max-w-[800px]"></div>
+        className="size-full max-h-[657px] max-w-[845px] rounded-lg border border-neutral-1000"></div>
     </div>
   );
 };
