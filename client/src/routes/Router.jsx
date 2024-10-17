40c385651ac1008f5a59021fae72516677cb47b9
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-10-16 21:39:02 -0300

    fix: route user signin

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -24,7 +24,7 @@ const AppRouter = () => {
     },
     {
       path: '/signin',
-      element: <EnterpriseSignup />,
+      element: <UserSigninPage />,
     },
     {
       path: '/enterprise-signup',
