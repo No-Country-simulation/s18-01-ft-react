24a0b43c02c715e72573afc9d96c8044dae35eda
thomilopez <thomasignaciolopez@gmail.com>
2024-10-24 16:56:04 -0300

    guardado de archivos en clourinary en los chats

--- server/src/controller/chat.controller.js
+++ server/src/controller/chat.controller.js
@@ -1,11 +1,25 @@
 const Message = require("../persistencia/models/messages.models.js");
 const parseEmotes = require("../utils/emoteParser.js");
+const cloudinary = require("../config/cloudinaryConfig.js");
 
 // Enviar un mensaje en una sala
 exports.sendMessage = async (req, res) => {
 	try {
 		let { message, senderId } = req.body;
-		const fileUrl = req.file ? `/uploads/${req.file.filename}` : null;
+		const fileUrl = null;
+
+		if (req.file) {
+			const result = await cloudinary.uploader
+				.upload_stream({ resource_type: "auto" }, (error, result) => {
+					if (error) {
+						throw new Error("Error al subir el archivo a Cloudinary");
+					}
+					return result;
+				})
+				.end(req.file.buffer); // Enviamos el buffer del archivo para su subida
+
+			fileUrl = result.secure_url; // Obtenemos la URL segura del archivo subido
+		}
 
 		// Parsear los emotes
 		message = parseEmotes(message);
