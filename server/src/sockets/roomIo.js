dada1e48f11d3049b176815888dca97906d32f3a
sibas1 <117487764+sibas1@users.noreply.github.com>
2024-10-29 12:38:29 -0500

    fix(soked):dublicacion usuario

--- server/src/sockets/roomIo.js
+++ server/src/sockets/roomIo.js
@@ -9,144 +9,96 @@ const handleSocketEvents = (io) => {
 	io.use(socketAuth);
 
 	const userChangeStream = User.watch();
-
 	userChangeStream.on("change", (change) => {
 		if (change.operationType === "update") {
 			const userId = change.documentKey._id;
 			const updatedFields = change.updateDescription.updatedFields;
-
 			if (updatedFields.status) {
-				io.emit("userStatusUpdate", {
-					userId: userId,
-					status: updatedFields.status,
-				});
+				io.emit("userStatusUpdate", { userId, status: updatedFields.status });
 			}
 		}
 	});
 
 	const notificationChangeStream = Notifications.watch();
-
 	notificationChangeStream.on("change", (change) => {
 		if (change.operationType === "insert") {
 			const newNotification = change.fullDocument;
-			const userId = newNotification.userId;
-
-			io.to(userId).emit("newNotification", newNotification);
+			io.to(newNotification.userId).emit("newNotification", newNotification);
 		}
 	});
 
-	// Change Stream para monitorear mensajes nuevos
 	const messageChangeStream = Messages.watch();
 	messageChangeStream.on("change", (change) => {
 		if (change.operationType === "insert") {
 			const newMessage = change.fullDocument;
-			const roomId = newMessage.roomId;
-
-			io.to(roomId).emit("newMessage", newMessage);
+			io.to(newMessage.roomId).emit("newMessage", newMessage);
 		}
 	});
 
-	// Change Stream para monitorear mensajes nuevos en los chats privados
 	const privateChatChangeStream = PrivateChat.watch();
-
 	privateChatChangeStream.on("change", (change) => {
-		if (
-			change.operationType === "update" &&
-			change.updateDescription.updatedFields["messages"]
-		) {
+		if (change.operationType === "update" && change.updateDescription.updatedFields.messages) {
 			const chatId = change.documentKey._id;
-			const newMessage =
-				change.updateDescription.updatedFields["messages"].slice(-1)[0]; // Obtener el último mensaje
-
-			const privateChat = change.fullDocument;
-			const participants = privateChat.participants;
-
-			// Emitir el nuevo mensaje a ambos participantes
-			participants.forEach((participant) => {
-				io.to(participant).emit("newPrivateMessage", { chatId, newMessage });
-			});
+			const newMessage = change.updateDescription.updatedFields.messages.slice(-1)[0];
+			const participants = change.fullDocument.participants;
+			participants.forEach(participant => io.to(participant).emit("newPrivateMessage", { chatId, newMessage }));
 		}
 	});
 
 	io.on("connection", (socket) => {
 		const user = socket.user;
-		//console.log(user);
-
-		User.findByIdAndUpdate(
-			user._id,
-			{ status: "online", socketId: socket.id },
-			{ new: true },
-		)
+		User.findByIdAndUpdate(user._id, { status: "online", socketId: socket.id }, { new: true })
 			.then(() => {
 				console.log(`${user.username} se ha conectado.`);
 				io.emit("userStatusUpdate", { userId: user._id, status: "online" });
 			})
 			.catch(console.error);
 
-		socket.on("joinRoom", async ({ roomId ,x,y}) => {
+		socket.on("joinRoom", async ({ roomId, x, y }) => {
 			try {
 				const room = await Rooms.findById(roomId);
-				//console.log(room, "sala");
-
 				if (!room) return;
 
 				socket.rooms.forEach((room) => {
 					if (room !== socket.id) {
 						socket.leave(room);
-						io.to(room).emit("userLeft", {
-							username: user.username,
-							userId: user._id,
-						});
+						io.to(room).emit("userLeft", { username: user.username, userId: user._id });
 					}
 				});
-				const newUser = { userId: user.id, socketId: socket.id, username: user.username, x, y }
-				room.users.push(newUser);
-				await room.save();
 
-				socket.join(roomId);
-				console.log(`${user.username} se ha unido a la sala ${roomId}.`);
+				const isUserInRoom = room.users.some(u => u.userId.toString() === user._id.toString());
+				if (!isUserInRoom) {
+					const newUser = { userId: user._id, socketId: socket.id, username: user.username, x, y };
+					room.users.push(newUser);
+					await room.save();
+					socket.join(roomId);
 
-				socket.emit("userList", room.users);
-				//socket.emit("newUserJoined", newUser);
-
-				socket.to(roomId).emit("newUserJoined", newUser);
-				console.log("Evento `newUserJoined` emitido con éxito");
-
-				io.to(room._id).emit("userCountUpdate", room.users.length);
+					socket.emit("userList", room.users);
+					socket.to(roomId).emit("newUserJoined", newUser);
+					io.to(room._id).emit("userCountUpdate", room.users.length);
+					console.log(`${user.username} se ha unido a la sala ${roomId}.`);
+				}
 			} catch (error) {
 				console.error("Error al unirse a la sala:", error);
 			}
 		});
 
 		socket.on("updatePosition", ({ x, y, prevDirection, direction }) => {
-			const roomId = [...socket.rooms][1]; // Obtener la primera sala a la que se unió
-
+			const roomId = Array.from(socket.rooms).find(room => room !== socket.id);
 			if (roomId) {
-				io.to(roomId).emit("userMoved", {
-					userId: user._id,
-					x,
-					y,
-					prevDirection,
-					direction,
-				});
+				io.to(roomId).emit("userMoved", { userId: user._id, x, y, prevDirection, direction });
 			}
 		});
 
 		socket.on("disconnect", async () => {
 			console.log(`${user.username} se ha desconectado.`);
-
 			try {
-				await User.findByIdAndUpdate(user._id, {
-					status: "offline",
-					socketId: "",
-				});
-
+				await User.findByIdAndUpdate(user._id, { status: "offline", socketId: "" });
 				const room = await Rooms.findOneAndUpdate(
 					{ "users.socketId": socket.id },
 					{ $pull: { users: { socketId: socket.id } } },
-					{ new: true },
+					{ new: true }
 				);
-
 				if (room) {
 					io.to(room._id).emit("userList", room.users);
 					io.to(room._id).emit("userCountUpdate", room.users.length);
