c880feb65d1b6c2b1f77005609de27bde59b269d
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-10-29 19:16:48 -0300

    fix: add fix base path

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -1,12 +1,10 @@
 import { useRoutes } from 'react-router-dom';
 import Office from '../views/Office/Office';
-import Home from '@/views/Home/Home';
 import NotFound from '@/views/NotFound/NotFound';
 import { UserSignupPage } from '@/views/UserSignup/UserSignupPage';
 import { UserSigninPage } from '@/views/UserSignin/UserSigninPage';
 import { UserWelcomePage } from '@/views/UserWelcome/UserWelcomePage';
 import { EnterpriseSignup } from '@/views/EnterpriseSignup/EnterpriseSignup';
-import { EnterpriseSignin } from '@/views/EnterpriseSignin/EnterpriseSignin';
 import { Outlet } from 'react-router-dom';
 import { ProtectedRoute } from './ProtectedRoute';
 import { OnlyNoAuth } from './OnlyNoAuth';
